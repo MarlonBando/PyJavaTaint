3374f8ee0a381fae66bc04e95d8ad29578588cd9
Mathiaz21 <mathias.gilbert42@gmail.com>
2024-11-09 16:42:46 +0100

    code refactoring

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from src.JVM_emulator import JVM_emulator
 
-file_path: str = './jpamb_examples/json_files/Simple.json'
-method_name: str = 'divideByZero'
+# file_path: str = './jpamb_examples/json_files/Simple.json'
+# method_name: str = 'divideByZero'
+
+file_path: str = './webgoat_code_examples/json_files/SqlInjectionLesson3.json'
+method_name: str = ''
 
 jvm_emulator: JVM_emulator = JVM_emulator(file_path, method_name)
 jvm_emulator.run()
