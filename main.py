84051c215ad6dcd9d6d88e8edb977704f4a74da6
Mathiaz21 <mathias.gilbert42@gmail.com>
2024-11-25 20:07:20 +0100

    Adding command line arguments to start the script

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from objects import Settings
 import query_list_generator as qlg
 from fuzzer import Fuzzer
+import sys
 
 settings = Settings()
-
 fuzzer = Fuzzer(settings)
-fuzzer.fuzz_all_endpoints()
-# fuzzer.fuzz_single_endpoint("Assignment 5a")
+
+if len(sys.argv) <= 2:
+  fuzzer.fuzz_all_endpoints()
+elif len(sys.argv) == 3:
+  lesson_name: str = sys.argv[2]
+  fuzzer.fuzz_single_endpoint(lesson_name)
+else:
+  raise ValueError("The fuzzer only accets one argument")
+
