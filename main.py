dd571ec639c482b9b56db7b8e700061e3e507d67
MarlonBando <michele.bandini2@studenti.unipr.it>
2024-11-28 10:41:40 +0100

    added parameter to main

--- main.py
+++ main.py
@@ -2,14 +2,23 @@ from objects import Settings
 import query_list_generator as qlg
 from fuzzer import Fuzzer
 import sys
+import argparse
 
+print("Starting PyJavaTaint fuzzer...")
+
+# Set up argument parser
+parser = argparse.ArgumentParser(description='PyJavaTaint Fuzzer')
+parser.add_argument('-e', '--endpoint', 
+           help='Endpoint name to fuzz. If omitted, all endpoints will be fuzzed. Available endpoints can be found in settings.yaml',
+           required=False)
+
+args = parser.parse_args()
 settings = Settings()
 fuzzer = Fuzzer(settings)
 
-if len(sys.argv) <= 2:
-  fuzzer.fuzz_all_endpoints()
-elif len(sys.argv) == 3:
-  lesson_name: str = sys.argv[2]
-  fuzzer.fuzz_single_endpoint(lesson_name)
+if args.endpoint:
+    print(f"  > Fuzzing single endpoint: {args.endpoint}")
+    fuzzer.fuzz_single_endpoint(args.endpoint)
 else:
-  raise ValueError("The fuzzer only accets one argument")
+    print("  > Fuzzing all endpoints...")
+    fuzzer.fuzz_all_endpoints()
